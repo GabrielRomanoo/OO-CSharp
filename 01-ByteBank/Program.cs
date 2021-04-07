e426ee662c99adfd98aa0c20c7b381434523e7a4
gabriel <gabriel.romano.3194@hotmail.com>
2021-04-07 10:16:39 -0300

    começando a modelar

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,6 +10,14 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
+            string titular = "Gabriela";
+            int numeroAgencia = 863;
+            int numero = 863146;
+            double saldo = 100;
+
+            string titular2 = "Bruno";
+            int numero2 = 864;
+            double saldo2 = 50;
         }
     }
 }
